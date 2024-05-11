6fc74ad75840d86884c771a1856eac3f384ef32b
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-05-12 03:35:12 +0530

    creating a dynamic component

---
+++ 03_React_essential/02_create_dynamic_component/src/App.jsx
@@ -0,0 +1,33 @@
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export function Header() {
+  const description = reactDescription[getRandomInt(2)];
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      {/* Here we have defined our componet 
+      that we have just build above using Header function */}
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
