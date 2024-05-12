eae787305fc1a0ba831b8bf8b5354a0aa4e63dc4
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-05-12 16:00:01 +0530

    loading img using {}

--- 03_React_essential/02_create_dynamic_component/src/App.jsx
+++ 03_React_essential/02_create_dynamic_component/src/App.jsx
@@ -1,4 +1,5 @@
 const reactDescription = ["Fundamental", "Crucial", "Core"];
+import image from "./assets/react-core-concepts.png";
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -8,7 +9,7 @@ export function Header() {
   const description = reactDescription[getRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={image} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are going to build!
