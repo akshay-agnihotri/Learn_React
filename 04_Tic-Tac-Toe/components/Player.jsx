e40afa3b1f1e1409234377525447757d206d82bb
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-06-02 19:51:45 +0530

    initiated the project Tic-Tac-Toe

---
+++ 04_Tic-Tac-Toe/components/Player.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+Player.propTypes = {
+  initialName: PropTypes.any.isRequired,
+  symbol: PropTypes.any.isRequired,
+};
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setisEditing] = useState(false);
+  const [playerName , setPlayerName] = useState(initialName);
+  function handleEditClick(){
+    setisEditing(editing=>!editing);
+  }
+  function updatePlayerName(event){
+    setPlayerName(event.target.value);
+  }
+  return (
+    <li>
+      <span className="player">
+        {isEditing && (<input type="text" required defaultValue={playerName} onChange={updatePlayerName}></input>)}
+        {!isEditing && (<span className="player-name">{playerName}</span>)}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button className="edit" onClick={handleEditClick}>{isEditing ? 'Save':'Edit'}</button>
+    </li>
+  );
+}
---
+++ 04_Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Player from '../components/Player.jsx';
+
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName='Player-1' symbol='X' />
+          <Player initialName='Player-2' symbol='0' />
+        </ol>
+      </div>
+    </main>
+  );
+}
+
+export default App;
