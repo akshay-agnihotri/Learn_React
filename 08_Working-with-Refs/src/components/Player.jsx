85f1cb65090915be89df83c3f9ac6e4b436ed61b
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-06-16 12:06:04 +0530

    completed the project

--- 08_Working-with-Refs/src/components/Player.jsx
+++ 08_Working-with-Refs/src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const [inputFieldValue, setInputFieldValue] = useState("");
+  const inputField = useRef();
+
+  function handleButtonClicked() {
+    setInputFieldValue(inputField.current.value);
+    inputField.current.value='';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>
+        Welcome {inputFieldValue === "" ? "unknown entity" : inputFieldValue}
+      </h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={inputField}  />
+        <button onClick={handleButtonClicked}>Set Name</button>
       </p>
     </section>
   );
