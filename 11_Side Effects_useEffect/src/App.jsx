6418b9e6cc45bcead5e7a4cb006e612a6e9f5da0
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-06-20 20:01:43 +0530

    added useEffect

--- 11_Side Effects_useEffect/src/App.jsx
+++ 11_Side Effects_useEffect/src/App.jsx
@@ -1,15 +1,28 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 
-import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
   const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      const sortedPlacesByDistance = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        pos.coords.latitude,
+        pos.coords.longitude
+      );
+
+      setPickedPlaces(sortedPlacesByDistance);
+    });
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -57,7 +70,7 @@ function App() {
       <main>
         <Places
           title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
+          fallbackText={"Select the places you would like to visit below."}
           places={pickedPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
