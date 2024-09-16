49da96a60be344a826e66a70c23739fbcf582c63
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-09-17 01:22:36 +0530

    More Redux Basics

--- 19_DivingIntoRedux/reduxBasics/redux-demo.js
+++ 19_DivingIntoRedux/reduxBasics/redux-demo.js
@@ -1,12 +1,22 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  return {
-    counter: state.counter + 1,
-  };
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
 };
 
-const store = redux.createStore(counterReducer); 
+const store = redux.createStore(counterReducer);
 // Redux internally triggers the reducer to initialize the state. Even though no action is dispatched explicitly, this is how Redux works — it ensures the store has an initial state by running the reducer once when the store is created.
 // The counterReducer is called with the default state { counter: 0 } during initialization.
 // The reducer increments the counter by 1 (because it always returns { counter: state.counter + 1 }), so the initial state of the store becomes { counter: 1 }.
@@ -19,4 +29,5 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: "increment" });
-console.log(store.getState());
+store.dispatch({ type: "decrement" });
+// console.log(store.getState());
