d2f88f17b5a57ab70906fe7fa7081e3f9d6787ac
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-09-17 20:58:26 +0530

    Working with Multiple State Properties

--- 19_DivingIntoRedux/reduxFirstProject/src/components/Counter.js
+++ 19_DivingIntoRedux/reduxFirstProject/src/components/Counter.js
@@ -3,24 +3,32 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch({ type: "increment", amount: 1 });
+  };
+
+  const incrementBy5Handler = () => {
+    dispatch({ type: "increment", amount: 5 });
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch({ type: "decrement", amount: 1 });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggleShowCounter" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={incrementBy5Handler}>Increase By 5</button>
         <button onClick={incrementHandler}>Increment</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
