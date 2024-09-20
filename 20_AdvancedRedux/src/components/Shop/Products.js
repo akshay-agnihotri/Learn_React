228b14cbee92817c2bf7452342a6e0a3cfdb27e4
akshay agnihotri <agnihotriakshay36@gmail.com>
2024-09-20 22:12:16 +0530

    completed functionality part without backend

--- 20_AdvancedRedux/src/components/Shop/Products.js
+++ 20_AdvancedRedux/src/components/Shop/Products.js
@@ -1,16 +1,43 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
-
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    title: "My First Book",
+    price: 6,
+    description: "This is the book i ever wrote",
+  },
+  {
+    id: "p2",
+    title: "My Second Book",
+    price: 5,
+    description: "This is the book i ever wrote",
+  },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
+        {DUMMY_PRODUCTS.map((productItem) => (
+          <ProductItem
+            key={productItem.id}
+            id={productItem.id}
+            title={productItem.title}
+            price={productItem.price}
+            description={productItem.description}
+          />
+        ))}
+        {/* <ProductItem
+          title="My First Book"
           price={6}
-          description='This is a first product - amazing!'
+          description="This is the book i ever wrote"
         />
+        <ProductItem
+          title="My Second Book"
+          price={5}
+          description="This is the book i ever wrote"
+        /> */}
       </ul>
     </section>
   );
--- 20_AdvancedRedux/src/store/cart-slice.js
+++ 20_AdvancedRedux/src/store/cart-slice.js
@@ -1,12 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { xyz: "something" };
+const initialState = { items: [], totalQuantity: 0, amount: 0 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    xyzfn(state, action) {},
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find(
+        (item) => item.itemId === newItem.id
+      );
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          totalPrice: newItem.price,
+          quantity: 1,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + existingItem.price;
+      }
+      state.totalQuantity++;
+    },
+    removeItemToCart(state, action) {
+      const itemId = action.payload;
+      const existingItem = state.items.find((item) => item.itemId === itemId);
+      state.totalQuantity--;
+      existingItem.totalPrice -= existingItem.price;
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.itemId !== itemId);
+      } else {
+        existingItem.quantity--;
+      }
+    },
   },
 });
 
