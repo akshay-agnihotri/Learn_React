23b5e81d409a204a633c089ef865ff1dd16cc7d1
akshay agnihotri <agnihotriakshay36@gmail.com>
2025-04-21 01:57:09 +0530

    added some change in 19 section

--- 19_DivingIntoRedux/reduxFirstProject/src/App.js
+++ 19_DivingIntoRedux/reduxFirstProject/src/App.js
@@ -2,6 +2,7 @@ import Counter from "./components/Counter.js";
 import Header from "./components/Header.js";
 import Auth from "./components/Auth.js";
 import { useSelector } from "react-redux";
+import  UserProfile  from "./components/UserProfile.js";
 
 function App() {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
@@ -9,6 +10,7 @@ function App() {
     <>
       {isAuthenticated && <Header />}
       {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
       {isAuthenticated && <Counter />}
     </>
   );
