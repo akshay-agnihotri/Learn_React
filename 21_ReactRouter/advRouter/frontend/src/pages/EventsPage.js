439dcc860002781e06d8c0fd02d9ee1beeec699f
akshay agnihotri <agnihotriakshay36@gmail.com>
2025-04-28 21:06:20 +0530

    382. Module Summary

--- 21_ReactRouter/advRouter/frontend/src/pages/EventsPage.js
+++ 21_ReactRouter/advRouter/frontend/src/pages/EventsPage.js
@@ -1,4 +1,4 @@
-import { Await, defer, json, useLoaderData } from "react-router-dom";
+import { Await, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 import { Suspense } from "react";
 
@@ -6,21 +6,20 @@ const loadEvents = async () => {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // throw new Response(JSON.stringify({ message: "could not fetch events" }), {
-    //   status: 500,
-    // });
-    throw json({ message: "could not fetch events" }, { status: 500 });
+    new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
     const resData = await response.json();
     return resData.events;
   }
 };
 
-export const eventsLoader = () => {
-  return defer({
+export async function eventsLoader() {
+  return {
     events: loadEvents(),
-  });
-};
+  };
+}
 
 function EventsPage() {
   const data = useLoaderData();
--- 21_ReactRouter/advRouter/frontend/src/pages/HomePage.js
+++ 21_ReactRouter/advRouter/frontend/src/pages/HomePage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PageContent from "./PageContent";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <PageContent title="Welcome!">
+      <p>Browse Our All amazing events </p>
+    </PageContent>
+  );
 }
 
 export default HomePage;
