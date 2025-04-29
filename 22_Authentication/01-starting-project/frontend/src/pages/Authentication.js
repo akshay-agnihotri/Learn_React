a3b112fd70ec8cd360ed31e86e0b8cb90641da3c
akshay agnihotri <agnihotriakshay36@gmail.com>
2025-04-30 01:24:54 +0530

    Play 398. Managing the Token Expiration

--- 22_Authentication/01-starting-project/frontend/src/pages/Authentication.js
+++ 22_Authentication/01-starting-project/frontend/src/pages/Authentication.js
@@ -33,9 +33,15 @@ export async function authAction({ request }) {
 
   const resData = await response.json();
   const token = resData.token;
+  
 
   localStorage.setItem("token", token);
 
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  
+  localStorage.setItem("expiration", expiration.toISOString());
+  
   return redirect("/");
 }
 
