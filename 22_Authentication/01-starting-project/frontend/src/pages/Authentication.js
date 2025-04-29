406e14da2412886fdb864bbd4eb5e5471037e329
akshay agnihotri <agnihotriakshay36@gmail.com>
2025-04-29 17:55:45 +0530

    389. Implementing the Auth Action

--- 22_Authentication/01-starting-project/frontend/src/pages/Authentication.js
+++ 22_Authentication/01-starting-project/frontend/src/pages/Authentication.js
@@ -1,7 +1,41 @@
-import AuthForm from '../components/AuthForm';
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
-function AuthenticationPage() {
+export async function authAction({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "Unstopped mode" }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authdata = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const response = fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(authdata),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response) {
+    throw json({ message: "Could not authenticate user." }, { status: 500 });
+  }
+
+  return redirect("/");
+}
+
+function Authentication() {
   return <AuthForm />;
 }
 
-export default AuthenticationPage;
+export default Authentication;
